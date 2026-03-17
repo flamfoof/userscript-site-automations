ba466e35281029b9dfa4ae4fd160e384a3de3015
Felix <felixlam0316@gmail.com>
2026-03-17 09:50:16 -0400

    Added other include tags for the site automation for weblinks delete

--- Admin Weblinks delete/Admin Weblinks delete.user.js
+++ Admin Weblinks delete/Admin Weblinks delete.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name		Admin Weblinks delete
-// @include		https://stvstaging.freecast.com/admin/guide/weblink/?*
+// @include		https://stvstaging.freecast.com/admin/guide/weblink/?*source__id__exact=31*
+// @include		https://stagingstv.freecast.com/admin/guide/weblink/?*source__id__exact=31*
 // @version  1
 // @grant    none
 // @grant    unsafeWindow
 // ==/UserScript==
 console.log("hi");
-let siteStatus = document.body.textContent.includes("502 Bad Gateway");
+let siteStatus =
+	document.body.textContent.includes("502 Bad Gateway") ||
+	document.body.textContent.includes("504 Gateway") ||
+	document.body.textContent.includes("503 Service");
 if (siteStatus) {
 	setTimeout(function () {
 		window.location.reload();
@@ -27,7 +31,7 @@ window.addEventListener(
 		}
 		console.log(idList);
 		console.log("started");
-        let fetchUrl = "https://stvstaging.freecast.com/admin/guide/weblink/?o=17&source__id__exact=764&updated_at__lte=2025-11-30#"
+		let fetchUrl = document.location.href;
 		let docCookieSessionToken = `sessionId=73vsuh52bemtif98hyvqjo2mjeawinfz;`;
 
 		let formInitialFetcher = null;
@@ -40,43 +44,43 @@ window.addEventListener(
 			let newSessionId = null;
 			let formInitCookie = `${docCookieSessionToken}${document.cookie}`;
 			console.log(formInitCookie);
-// 			formInitialFetcher = await fetch(
-// 				fetchUrl,
-// 				{
-// 					headers: {
-// 						"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0",
-// 						Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-// 						"Accept-Language": "en-US,en;q=0.5",
-// 						"Upgrade-Insecure-Requests": "1",
-// 						"Sec-Fetch-Dest": "document",
-// 						"Sec-Fetch-Mode": "navigate",
-// 						"Sec-Fetch-Site": "none",
-// 						"Sec-Fetch-User": "?1",
-// 						Priority: "u=0, i",
-// 						cookie: formInitCookie,
-// 					},
-// 					referrer: window.location.href, // Top-level property
-// 					referrerPolicy: "no-referrer-when-downgrade", // Top-level property
-// 					method: "GET",
-// 					mode: "cors",
-// 				},
-// 			).then((response) => {
-// 				return response.text();
-// 			});
+			// 			formInitialFetcher = await fetch(
+			// 				fetchUrl,
+			// 				{
+			// 					headers: {
+			// 						"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0",
+			// 						Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+			// 						"Accept-Language": "en-US,en;q=0.5",
+			// 						"Upgrade-Insecure-Requests": "1",
+			// 						"Sec-Fetch-Dest": "document",
+			// 						"Sec-Fetch-Mode": "navigate",
+			// 						"Sec-Fetch-Site": "none",
+			// 						"Sec-Fetch-User": "?1",
+			// 						Priority: "u=0, i",
+			// 						cookie: formInitCookie,
+			// 					},
+			// 					referrer: window.location.href, // Top-level property
+			// 					referrerPolicy: "no-referrer-when-downgrade", // Top-level property
+			// 					method: "GET",
+			// 					mode: "cors",
+			// 				},
+			// 			).then((response) => {
+			// 				return response.text();
+			// 			});
 			console.log("init fetching");
-// 			let initialBody = formInitialFetcher;
-// 			const initDoc = parser.parseFromString(initialBody, "text/html");
-// 			console.log(initDoc);
+			// 			let initialBody = formInitialFetcher;
+			// 			const initDoc = parser.parseFromString(initialBody, "text/html");
+			// 			console.log(initDoc);
 			let csrfFormToken = document.getElementById("changelist-form").children[0].value;
 			let initFormRequestStart = "csrfmiddlewaretoken=" + csrfFormToken + "&action=delete_selected&select_across=0&index=0";
-// 			let initFormRequestMiddle = "";
-// 			idList.forEach((id) => {
-// 				initFormRequestMiddle += `&_selected_action=${id}`;
-// 			});
-// 			let bodyInitFormRequest = initFormRequestStart + initFormRequestMiddle;
-// 			console.log(bodyInitFormRequest);
-// 			let formGetFetcherCookie = `${docCookieSessionToken}${this.document.cookie}`;
-// 			console.log(formGetFetcherCookie);
+			// 			let initFormRequestMiddle = "";
+			// 			idList.forEach((id) => {
+			// 				initFormRequestMiddle += `&_selected_action=${id}`;
+			// 			});
+			// 			let bodyInitFormRequest = initFormRequestStart + initFormRequestMiddle;
+			// 			console.log(bodyInitFormRequest);
+			// 			let formGetFetcherCookie = `${docCookieSessionToken}${this.document.cookie}`;
+			// 			console.log(formGetFetcherCookie);
 
 			// formGetFetcher = await fetch(
 			// 	fetchUrl,
@@ -104,7 +108,7 @@ window.addEventListener(
 			// let getFetcherBody = await formGetFetcher.text();
 			// const getFetcherDoc = parser.parseFromString(initialBody, "text/html");
 			// console.log(getFetcherDoc);
-			let getFetcherCsrf = csrfFormToken
+			let getFetcherCsrf = csrfFormToken;
 
 			let formSendRequestStart = "csrfmiddlewaretoken=" + csrfFormToken;
 			let formSendRequestMiddle = "";
@@ -116,10 +120,8 @@ window.addEventListener(
 			console.log(bodyFormSendRequest);
 			let formSendFetcherCookie = formInitCookie;
 			console.log(formSendFetcherCookie);
-
-			formSendFetcher = await fetch(
-				fetchUrl,
-				{
+			try {
+				formSendFetcher = await fetch(fetchUrl, {
 					headers: {
 						"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0",
 						Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -137,8 +139,10 @@ window.addEventListener(
 					mode: "cors",
 					referrer: window.location.href, // Top-level property
 					referrerPolicy: "no-referrer-when-downgrade", // Top-level property
-				},
-			);
+				});
+			} catch (e) {
+				console.log(e);
+			}
 			console.log("final fetching");
 			console.log(formSendFetcher);
 			setTimeout(function () {
